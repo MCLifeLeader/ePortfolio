21c01191cfaf82f3eba96b4ace540991c91fb782
Michael Carey <Michael@AGameEmpowerment.com>
2020-11-05 17:57:23 -0700

    Updated Nuget packages for Bootstrap and log4net. Added a fun projects section page. This first page is about how I am building a Ham Radio Go-Kit.

---
+++ Portfolio/Portfolio/Controllers/FunProjectsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using log4net;
+
+namespace Portfolio.Controllers
+{
+   public class FunProjectsController : Controller
+   {
+      #region Properties
+
+      private readonly ILog _logger = LogManager.GetLogger(typeof(FunProjectsController));
+
+      #endregion
+
+      public ActionResult Index()
+      {
+         _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Index));
+
+         return View();
+      }
+
+      public ActionResult HamRadioVHFGoKit()
+      {
+         _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Index));
+
+         return View();
+      }
+   }
+}
