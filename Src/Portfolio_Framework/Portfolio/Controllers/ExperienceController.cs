8a7bafc5e72fe7285df93838d9593c48c766784a
Michael Carey <Michael@AGameEmpowerment.com>
2021-04-06 14:46:52 -0600

    Updating my ePortfolio to have both a Work History and Education section each.

--- Src/Portfolio_Framework/Portfolio/Controllers/ExperienceController.cs
+++ Src/Portfolio_Framework/Portfolio/Controllers/ExperienceController.cs
@@ -3,8 +3,8 @@ using log4net;
 
 namespace Portfolio.Controllers
 {
-	public class ExperienceController : Controller
-	{
+   public class ExperienceController : Controller
+   {
       #region Properties
 
       private readonly ILog _logger = LogManager.GetLogger(typeof(ExperienceController));
@@ -12,10 +12,24 @@ namespace Portfolio.Controllers
       #endregion
 
       public ActionResult Index()
-		{
+      {
          _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Index));
 
          return View();
-		}
-	}
+      }
+
+      public ActionResult WorkHistory()
+      {
+         _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(WorkHistory));
+
+         return View();
+      }
+
+      public ActionResult Education()
+      {
+         _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Education));
+
+         return View();
+      }
+   }
 }
