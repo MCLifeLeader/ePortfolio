68758484a3882749d94b9940ddd3213019396d3f
Michael Carey <Michael@AGameEmpowerment.com>
2021-08-25 19:53:49 -0600

    Updated the project to .NET Framework 4.8 and updated nuget packages to match updated Framework. Adjusted BundleConfig setup.

--- Src/Portfolio_Framework/Portfolio/Scripts/_references.js
+++ Src/Portfolio_Framework/Portfolio/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="jquery-3.6.0.js" />
-/// <reference path="modernizr-2.8.3.js" />
+﻿/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="jquery-3.6.0.js" />
 /// <autosync enabled="true" />
 /// <reference path="bootstrap.min.js" />
 /// <reference path="jquery.validate.min.js" />
