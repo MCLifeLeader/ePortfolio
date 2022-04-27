d283c8a77aabfd607156522bd46f2fc952b01363
Michael Carey <Michael@AGameEmpowerment.com>
2022-04-26 20:37:15 -0600

    Added a food calculator from another website. I'm going to finish cleaning it up and make it run on my site.

--- Src/Portfolio_Framework/Portfolio/Controllers/HomeController.cs
+++ Src/Portfolio_Framework/Portfolio/Controllers/HomeController.cs
@@ -10,79 +10,86 @@ using RestSharp;
 
 namespace Portfolio.Controllers
 {
-   public class HomeController : Controller
-   {
-      private readonly ILog _logger = LogManager.GetLogger(typeof(HomeController));
+    public class HomeController : Controller
+    {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(HomeController));
 
-      public ActionResult Index()
-      {
-         _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Index));
+        public ActionResult Index()
+        {
+            _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Index));
 
-         return View();
-      }
-
-      public ActionResult About()
-      {
-         _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(About));
-
-         return View();
-      }
-
-      [HttpGet]
-      public ActionResult Contact()
-      {
-         _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Contact));
-
-         return View();
-      }
-
-      [HttpPost]
-      public async Task<ActionResult> Contact(EmailMessage emailMessage, FormCollection form)
-      {
-         _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Contact));
-
-         ViewBag.Message = "Message Failed";
-
-         string userResponse = HttpContext.Request.Params["g-recaptcha-response"];
-         if (!await ReCaptcha.ValidateCaptchaAsync(userResponse))
-         {
-            ViewBag.Message = "Please Try again. Captcha not valid";
-            _logger.WarnFormat("Captia was not valid: {0}", userResponse);
-            // Bot Attack, non validated !
             return View();
-         }
-         
-         if (ModelState.IsValid)
-         {
-            if (!string.IsNullOrEmpty(emailMessage.FromAddress) && !string.IsNullOrEmpty(emailMessage.MessageContent))
+        }
+
+        public ActionResult About()
+        {
+            _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(About));
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Contact));
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Contact(EmailMessage emailMessage, FormCollection form)
+        {
+            _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Contact));
+
+            ViewBag.Message = "Message Failed";
+
+            string userResponse = HttpContext.Request.Params["g-recaptcha-response"];
+            if (!await ReCaptcha.ValidateCaptchaAsync(userResponse))
             {
-               // Get the secret key value from 
-               string appKey = Environment.GetEnvironmentVariable("AGameFunctionAppKey");
-
-               emailMessage.Subject = "Contact Form: mbcarey.com";
-
-               RestClient client = new RestClient(Settings.Default.AGameWebUrl);
-               RestRequest request = new RestRequest($"api/PersonalEmail?code={appKey}", Method.POST, DataFormat.Json);
-               request.AddJsonBody(await emailMessage.ToJsonAsync());
-               IRestResponse response = await client.ExecuteAsync(request);
-
-               if (response.StatusCode == HttpStatusCode.OK)
-               {
-                  ViewBag.Message = "Message Sent";
-               }
+                ViewBag.Message = "Please Try again. Captcha not valid";
+                _logger.WarnFormat("Captia was not valid: {0}", userResponse);
+                // Bot Attack, non validated !
+                return View();
             }
-         }
 
-         ModelState.Clear();
+            if (ModelState.IsValid)
+            {
+                if (!string.IsNullOrEmpty(emailMessage.FromAddress) && !string.IsNullOrEmpty(emailMessage.MessageContent))
+                {
+                    // Get the secret key value from 
+                    string appKey = Environment.GetEnvironmentVariable("AGameFunctionAppKey");
 
-         return View();
-      }
+                    emailMessage.Subject = "Contact Form: mbcarey.com";
 
-      public ActionResult Git()
-      {
-         _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Git));
+                    RestClient client = new RestClient(Settings.Default.AGameWebUrl);
+                    RestRequest request = new RestRequest($"api/PersonalEmail?code={appKey}", Method.POST, DataFormat.Json);
+                    request.AddJsonBody(await emailMessage.ToJsonAsync());
+                    IRestResponse response = await client.ExecuteAsync(request);
 
-         return View();
-      }
-   }
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        ViewBag.Message = "Message Sent";
+                    }
+                }
+            }
+
+            ModelState.Clear();
+
+            return View();
+        }
+
+        public ActionResult Git()
+        {
+            _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Git));
+
+            return View();
+        }
+
+        public ActionResult FoodStorageCalculator()
+        {
+            _logger.DebugFormat("'{0}.{1}' called", GetType().Name, nameof(Git));
+
+            return View();
+        }
+    }
 }
