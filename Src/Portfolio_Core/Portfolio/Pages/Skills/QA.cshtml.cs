2d4c914bbbf92193bef063cbd23d8726dc9f42c7
Michael Carey <Michael@AGameEmpowerment.com>
2023-10-01 18:48:44 -0600

    Migrated more pages from the legacy project to the updated project.

---
+++ Src/Portfolio_Core/Portfolio/Pages/Skills/QA.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages.Skills
+{
+    public class QAModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
